0d34cc3ca61a5406511029efc27d90a09c1734ad
nishu959 <60173728+nishu959@users.noreply.github.com>
2021-05-11 11:45:45 +0530

    Add files via upload

---
+++ NGL.py
@@ -0,0 +1,20 @@
+def solve(arr, s, v):
+  for i in range(0,len(arr),1):
+    if len(s)==0:
+      v.append(-1)
+    elif (len(s)>0 and s[-1]>arr[i]):
+      v.append(s[-1])
+    elif (len(s)>0 and s[-1]<arr[i]):
+      while(len(s)>0 and s[-1]<=arr[i]):
+        s.pop()
+      if len(s)==0:
+        v.append(-1)
+      else:
+        v.append(s[-1])
+    s.append(arr[i])
+  return v
+ 
+arr = list(map(int, input().split()))
+v = []
+
+print(solve(arr,[],v))
---
+++ NGR.py
@@ -0,0 +1,21 @@
+def solve(arr, s, v):
+  for i in range(len(arr) - 1,-1,-1):
+    if len(s)==0:
+      v.append(-1)
+    elif (len(s)>0 and s[-1]>arr[i]):
+      v.append(s[-1])
+    elif (len(s)>0 and s[-1]<arr[i]):
+      while(len(s)>0 and s[-1]<=arr[i]):
+        s.pop()
+      if len(s)==0:
+        v.append(-1)
+      else:
+        v.append(s[-1])
+    s.append(arr[i])
+  v.reverse()
+  return v
+ 
+arr = list(map(int, input().split()))
+v = []
+
+print(solve(arr,[],v)) 
