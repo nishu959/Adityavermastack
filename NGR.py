db50dbfbd1a22132e0252997462f14e5f8dfee08
nishu959 <60173728+nishu959@users.noreply.github.com>
2021-05-11 16:18:54 +0530

    Update NGR.py

--- NGR.py
+++ NGR.py
@@ -4,7 +4,7 @@ def solve(arr, s, v):
       v.append(-1)
     elif (len(s)>0 and s[-1]>arr[i]):
       v.append(s[-1])
-    elif (len(s)>0 and s[-1]<arr[i]):
+    elif (len(s)>0 and s[-1]<=arr[i]):
       while(len(s)>0 and s[-1]<=arr[i]):
         s.pop()
       if len(s)==0:
