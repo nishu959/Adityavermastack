01c9b2a3b2a73c6a5252c4bea2f13454e5fc5ec7
nishu959 <60173728+nishu959@users.noreply.github.com>
2023-03-12 15:21:48 +0530

    Update NGL.py

--- NGL.py
+++ NGL.py
@@ -1,20 +1,27 @@
-def solve(arr, s, v):
-  for i in range(0,len(arr),1):
-    if len(s)==0:
-      v.append(-1)
-    elif (len(s)>0 and s[-1]>arr[i]):
-      v.append(s[-1])
-    elif (len(s)>0 and s[-1]<=arr[i]):
-      while(len(s)>0 and s[-1]<=arr[i]):
-        s.pop()
-      if len(s)==0:
-        v.append(-1)
-      else:
-        v.append(s[-1])
-    s.append(arr[i])
-  return v
- 
-arr = list(map(int, input().split()))
-v = []
 
-print(solve(arr,[],v))
+
+arr=[1,3,2,4,-1,9,5,10,6]       
+stack = []
+ans = []
+n = len(arr)
+
+for i in range(0, n):
+    
+    if len(stack)==0:
+        ans.append(-1)
+        
+    else:
+        while len(stack)>0 and stack[-1]<=arr[i]:
+            stack.pop()
+    
+                
+        if len(stack)==0:
+            ans.append(-1)
+        else:
+            ans.append(stack[-1])
+            
+        
+        
+    stack.append(arr[i])
+    
+print(ans)
