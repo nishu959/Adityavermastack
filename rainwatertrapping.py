0a64f18e99168afb9f06e08ba07cbeb32cfb8b7f
nishu959 <60173728+nishu959@users.noreply.github.com>
2023-03-14 16:40:08 +0530

    Update rainwatertrapping.py

--- rainwatertrapping.py
+++ rainwatertrapping.py
@@ -14,3 +14,35 @@ for i in range(len(a)):
   width[i] = min(mxl[i], mxr[i]) - a[i]
  
 print(sum(width)) 
+
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        l = 0
+        r = n-1
+        leftmax = 0
+        rightmax=0
+        res=0
+
+        while l<=r:
+            if height[l] <= height[r]:
+                
+                if height[l]>=leftmax:
+                    leftmax = max(leftmax, height[l])
+                else:
+                    res += leftmax-height[l]
+
+                l += 1
+
+            else:
+
+                if height[r]>=rightmax:
+                    rightmax = max(rightmax , height[r])
+                else:
+                    res += rightmax-height[r]
+
+                r -= 1
+
+        return res
