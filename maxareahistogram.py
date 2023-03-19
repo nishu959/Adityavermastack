6983078032e7bc966d809d6ec652b7697b4d7986
nishu959 <60173728+nishu959@users.noreply.github.com>
2023-03-20 00:23:06 +0530

    Update maxareahistogram.py

--- maxareahistogram.py
+++ maxareahistogram.py
@@ -1,58 +1,39 @@
-def solve1(arr, s, v):
-  for i in range(0,len(arr),1):
-    if len(s)==0:
-      v.append(-1)
-    elif (len(s)>0 and s[-1][0]<arr[i]):
-      v.append(s[-1][1])
-    elif (len(s)>0 and s[-1][0]>=arr[i]):
-      while(len(s)>0 and s[-1][0]>=arr[i]):
-        s.pop()
-      if len(s)==0:
-        v.append(-1)
-      else:
-        v.append(s[-1][1])
-    s.append([arr[i], i])
-  return v
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        stack = []
+        NSL = []
+        n = len(heights)
+        for i in range(n):
+            
+            while len(stack)>0 and stack[-1][0]>=heights[i]:
+                stack.pop()
 
+            if len(stack)==0:
+                NSL.append(-1)
+            else:
+                NSL.append(stack[-1][1])
 
-def solve2(arr, s, v):
-  for i in range(len(arr)-1,-1,-1):
-    if len(s)==0:
-      v.append(len(arr))
-    elif (len(s)>0 and s[-1][0]<arr[i]):
-      v.append(s[-1][1])
-    elif (len(s)>0 and s[-1][0]>=arr[i]):
-      while(len(s)>0 and s[-1][0]>=arr[i]):
-        s.pop()
-      if len(s)==0:
-        v.append(len(arr))
-      else:
-        v.append(s[-1][1])
-    s.append([arr[i], i])
-  v.reverse()
-  return v
-     
-arr = list(map(int, input().split()))
-v = []
-q =solve1(arr,[],v)
-v = []
-p = solve2(arr, [], v)
-right = p
-left = q
-width = []
-#NSR and NSL
-print(right)
-print(left)
-for i in range(len(p)):
-  width.append(right[i]-left[i]-1) 
-#width
-print(width)
-area=[]  
-  
-for i in range(len(arr)):
-  area.append(arr[i] * width[i]) 
-#area
-print(area)  
-#maxarea
-print(max(area)) 
-  
+            stack.append([heights[i], i])
+
+        stack=[]
+        NSR=[]
+        n = len(heights)
+
+        for i in range(n-1, -1, -1):
+            
+            while len(stack)>0 and stack[-1][0]>=heights[i]:
+                stack.pop()
+            if len(stack)==0:
+                NSR.append(n)
+            else:
+                NSR.append(stack[-1][1])
+
+            stack.append([heights[i],i])
+
+        # print(NSL)
+        NSR= NSR[::-1]
+
+        area = -sys.maxsize
+        for i in range(n):
+            area = max(area , (NSR[i]-NSL[i]-1)*heights[i])
+        return area
