e4b7336a2d4445e83adb356f0d072712e495d0b3
nishu959 <60173728+nishu959@users.noreply.github.com>
2023-03-20 00:25:11 +0530

    Update NSL.py

--- NSL.py
+++ NSL.py
@@ -7,19 +7,16 @@ class Solution:
         
         for i in range(n):
             
+            
+                
+            while len(stack)>0 and stack[-1] >=a[i]:
+                stack.pop()
+                
             if len(stack)==0:
                 ans.append(-1)
-                
             else:
+                ans.append(stack[-1])
                 
-                while len(stack)>0 and stack[-1] >=a[i]:
-                    stack.pop()
-                    
-                if len(stack)==0:
-                    ans.append(-1)
-                else:
-                    ans.append(stack[-1])
-                    
             stack.append(a[i])
                     
         return ans
